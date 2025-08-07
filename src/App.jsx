50ee40342e2a82bcce258e7c70625d095bf751be
siddheshtodkar <stodkar53@gmail.com>
2025-08-07 20:25:18 +0530

    fetch and display coins

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import Coin from "./components/coin";
+const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
 const App = () => {
-  return ( <>Crypto Dash</> );
+  const [coins, setCoins] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(API_URL)
+        if (!res.ok)
+          throw new Error('fetch coins failed')
+        const data = await res.json()
+        console.table(data)
+        setCoins(data)
+      } catch (error) {
+        setError(error.message)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchCoins()
+  }, [])
+
+  return (
+    <div>
+      <h1>Crypto Dash</h1>
+      {loading && <p>Loading...</p>}
+      {error && <div className="error">{error}</div>}
+      {!loading && !error &&
+        <main className="grid">
+          {coins.map(coin => (
+            <Coin coin={coin} key={coin.id} />
+          ))}
+        </main>
+      }
+    </div>
+  );
 }
- 
+
 export default App;
